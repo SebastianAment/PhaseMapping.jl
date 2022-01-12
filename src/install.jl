fd4e758f0a06b65c1aef4cb39251fd779c1af74d
Sebastian Ament <sebastianeament@gmail.com>
2022-01-12 13:16:39 +0100

    updated install file

--- src/install.jl
+++ src/install.jl
@@ -9,22 +9,6 @@ end
 
 add(x) = Pkg.add(Pkg.PackageSpec(url = git * x * ".git"))
 
-add("LazyInverse.jl")
-add("LinearAlgebraExtensions.jl")
-add("WoodburyIdentity.jl")
-add("KroneckerProducts.jl")
 add("OptimizationAlgorithms.jl")
-add("CovarianceFunctions.jl")
-add("CompressedSensing.jl")
 add("BackgroundSubtraction.jl")
 add("PhaseMapping.jl")
-
-# Pkg.rm("LazyInverse.jl")
-# Pkg.rm("LinearAlgebraExtensions.jl")
-# Pkg.rm("WoodburyIdentity.jl")
-# Pkg.rm("KroneckerProducts.jl")
-# Pkg.rm("OptimizationAlgorithms.jl")
-# Pkg.rm("CovarianceFunctions.jl")
-# Pkg.rm("CompressedSensing.jl")
-# Pkg.rm("BackgroundSubtraction.jl")
-# Pkg.rm("PhaseMapping.jl")
